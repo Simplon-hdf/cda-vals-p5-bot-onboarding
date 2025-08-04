15261150880c7c03064c7913fab89e5be51bdeaa
SauroMatheo <sauromatheo@gmail.com>
2025-08-04 16:41:26 +0200

    feat(identificationValidation): accepting or rejecting now edits the request

--- src/handlers/identificationValidation.ts
+++ src/handlers/identificationValidation.ts
@@ -1,4 +1,4 @@
-import { Interaction, Client, TextChannel } from "discord.js";
+import { Interaction, Client, TextChannel, APIEmbed, ButtonInteraction, MessageFlags } from "discord.js";
 
 import wait from 'node:timers/promises'
 
@@ -9,28 +9,52 @@ export async function identificationValidation(interaction: Interaction, client:
         // TODO: Call the backend API to accept the identification request
 		await interaction.reply({
 			content: '✅ Demande acceptée.',
-			ephemeral: true,
+			flags: [MessageFlags.Ephemeral],
 		});
 
-        const editedEmbed = interaction.message.embeds[0].toJSON();
-        editedEmbed.color = 0x00ff00;
-        interaction.message.edit(
-            { embeds: [editedEmbed] }
-        ); // Change the embed color to green
-
-        //await interaction.message.delete(); // Original message
-        await wait.setTimeout(10000); // Wait for 10 seconds before deleting the reply message
-        await interaction.deleteReply();
+        await editEmbed(
+            interaction,
+            0x00f020,
+            `✅ Demande acceptée par ${interaction.user.tag}`,
+        );
 
 	} else if (interaction.customId === 'rejectIdentification') {
         // TODO: Call the backend API to reject the identification request
 		await interaction.reply({
 			content: '❌ Demande refusée.',
-			ephemeral: true,
+			flags: [MessageFlags.Ephemeral],
 		});
 
-        await interaction.message.delete(); // Original message
-        await wait.setTimeout(10000); // Wait for 10 seconds before deleting the reply message
-        await interaction.deleteReply();
-	}
+        await editEmbed(
+            interaction,
+            0xf01010,
+            `❌ Demande refusée par ${interaction.user.tag}`,
+        );
+
+	} else { return; }
+
+    // await interaction.message.delete(); // Original message
+    await wait.setTimeout(10000); // Wait for 10 seconds before deleting the reply message
+    await interaction.deleteReply();
+}
+
+
+async function editEmbed(interaction: ButtonInteraction, color: number, footerText: string): Promise<APIEmbed> {
+    let editedEmbed = {
+        ...interaction.message.embeds[0].toJSON(),
+        color: color,
+        footer: {
+            text: footerText
+        }
+    };
+    
+    interaction.message.edit(
+        { 
+            content: `-# La demande de ${interaction.message.mentions.users.first()} a été traitée par ${interaction.user}`,
+            embeds: [editedEmbed],
+            components: [],
+        }
+    );
+
+    return editedEmbed;
 }
