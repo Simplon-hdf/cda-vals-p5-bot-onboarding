de35ffa9688a7f785a91a9ccead44367c5eb564b
SauroMatheo <sauromatheo@gmail.com>
2025-08-11 14:37:37 +0200

    chore(messageRelay): make the channels use the channels in config

--- src/handlers/messageRelay.ts
+++ src/handlers/messageRelay.ts
@@ -1,17 +1,18 @@
 import { Message, Client, TextChannel, EmbedBuilder, ContainerBuilder, ButtonStyle, MessageFlags, ActionRowBuilder, ButtonBuilder } from "discord.js";
+import { ConfigManager } from "../config/ConfigManager";
 
 export async function handleRelay(message: Message, client: Client) {
-	const fromChannelId = process.env.RELAY_SOURCE_CHANNEL;
-	const toChannelId = process.env.RELAY_TARGET_CHANNEL;
+	const fromChannelId = ConfigManager.config?.salonIdentificationId;
+	const toChannelId = ConfigManager.config?.salonIdentificationStaffId;
 
 	if (!fromChannelId || !toChannelId) {
-		console.error("RELAY_SOURCE or RELAY_DEST environment variables are not set.");
+		console.error("[messageRelay] Source or destination channel ID is not set. (salonIdentificationId or salonIdentificationStaffId)");
 		return;
 	}
 
 	if (message.channel.id !== fromChannelId || message.author.bot) return;
 
-    console.log(`messageRelay> Message received: ${message.content} from ${message.author.tag}`);
+    console.log(`[messageRelay] Message received: ${message.content} from ${message.author.tag}`);
 
 	const targetChannel = await client.channels.fetch(toChannelId);
 	if (!targetChannel?.isTextBased()) return;
